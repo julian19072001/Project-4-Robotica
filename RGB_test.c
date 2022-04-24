d3c271beb3229f57067341f7842c6c7fd51f04f5
Julian Della Guardia <juliandellaguardia@hotmail.com>
2022-04-24 17:46:55 +0200

    RBG test aangepast

--- RGB_test.c
+++ RGB_test.c
@@ -5,87 +5,221 @@
 #include <avr/interrupt.h>
 #include "serialF0.h"
 #include "clock.h"
+#include "Kleur_arrays.h"
 
-int led_out_green[96] = {0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,
-						 0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,
-						 0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,
-						 0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,
-						 0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,
-						 0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0};
-	
-int led_out_red[96] = 	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-						 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,
-						 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-						 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-						 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,
-						 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	
-int led_out_blue[96] = 	{0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,
-						 0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,
-						 0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,
-						 0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,
-						 0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,
-						 0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0};
-	
-int led_out_ir[96] = 	{1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,
-						 1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,
-						 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-						 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-						 1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,
-						 1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0};
+#define SAMPLES 8
+
+static volatile int16_t res_d75, res_d72, res_d69, res_d66, res_d63, res_d60, res_d57;
 
 void init_Line_Follower(void);
+void init_Timer(void);
+void color_Line_Follower(void);
 
-int main(void) {
+int main(void) 
+{
+	init_Line_Follower();
+	init_Timer();
 	init_clock();
 	init_stream(F_CPU);
-	init_Line_Follower();
-	
-	for (int i = 0; i < 96; i++) {
-		if (led_out_ir[i] == 1){
-			PORTE_OUTSET = PIN7_bm;
-		} else {
-			PORTE_OUTCLR = PIN7_bm;
-		}
-		
-		PORTE_OUTTGL = PIN5_bm;
-		PORTE_OUTTGL = PIN5_bm;
-	}
-	
-	// 1 puls op LAT
-	PORTE_OUTTGL = PIN4_bm;
-	PORTE_OUTTGL = PIN4_bm;
-	
-	PORTE_OUTCLR = PIN3_bm;		// BLANK low
-
-	PORTA.DIRCLR     = PIN0_bm;															//configure PA0 as input for ADCA
-	ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN0_gc | ADC_CH_MUXNEG_GND_MODE3_gc;				//PA0 to channel 0
-	ADCA.CH0.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;										//differential, no gain
-
-	ADCA.CTRLB       =	ADC_RESOLUTION_12BIT_gc | ADC_CONMODE_bm | ADC_FREERUN_bm; 		//free running mode
-
-	ADCA.REFCTRL     =  ADC_REFSEL_INTVCC_gc;											//internal vcc/1.6 refernce
-	ADCA.PRESCALER   =  ADC_PRESCALER_DIV16_gc;											//prescaling
-	ADCA.CTRLA       =  ADC_ENABLE_bm;													//enable ADC
 
 	sei(); 
 
-	PMIC.CTRL |= PMIC_LOLVLEN_bm;
-
-	int val;
-	while (1) {
-	ADCA.CH0.CTRL |= ADC_CH_START_bm;                  						//start ADC conversion
-	while ( !(ADCA.CH0.INTFLAGS & ADC_CH_CHIF_bm) ) ;   					//wait until it's ready
-	val = ADCA.CH0.RES;														//store the ADC value
-	ADCA.CH0.INTFLAGS |= ADC_CH_CHIF_bm;                					//reset interrupt flag
-	printf("%d", val);
+	while (1) 
+	{
+		
+		printf("\n");
+		printf("%5d | %5d | %5d | %5d | %5d | %5d | %5d", res_d75, res_d72, res_d69, res_d66, res_d63, res_d60, res_d57);
+		printf("\n");
+		_delay_ms(100);
 	}
+}
+
+//Interrupt service routine for d75
+ISR(ADCA_CH0_vect)
+{
+  static uint8_t n_d75 = 0;
+  static int16_t sum_d75 = 0;
+
+  if(n_d75 & 0x01) 
+  {                  		//second (even) measurement
+    sum_d75 -= ADCA.CH0.RES;
+    ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN1_gc | ADC_CH_MUXNEG_PIN3_gc;
+  } 
+  else 
+  {                         //first (odd) measurement
+    sum_d75 += ADCA.CH0.RES;
+    ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_PIN1_gc;
+  }
+
+  n_d75++;
+  if(n_d75 == SAMPLES) 
+  {							//if eight measurement have been made safe it as a result
+    res_d75 = sum_d75/SAMPLES;
+    sum_d75 = 0;
+    n_d75 = 0;
+  }
+}
+
+//Interrupt service routine for d72
+ISR(ADCA_CH1_vect)
+{
+  static uint8_t n_d72 = 0;
+  static int16_t sum_d72 = 0;
+
+  if(n_d72 & 0x01) 
+  {                  		//second (even) measurement
+    sum_d72 -= ADCA.CH1.RES;
+    ADCA.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN4_gc | ADC_CH_MUXNEG_PIN3_gc;
+  } 
+  else 
+  {                         //first (odd) measurement
+    sum_d72 += ADCA.CH1.RES;
+    ADCA.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_PIN4_gc;
+  }
+
+  n_d72++;
+  if(n_d72 == SAMPLES)
+  {							//if eight measurement have been made safe it as a result
+    res_d72 = sum_d72/SAMPLES;
+    sum_d72 = 0;
+    n_d72 = 0;
+  }
+}
+
+//Interrupt service routine for d69
+ISR(ADCA_CH2_vect)
+{
+  static uint8_t n_d69 = 0;
+  static int16_t sum_d69 = 0;
+
+  if(n_d69 & 0x01) 
+  {                  		//second (even) measurement
+    sum_d69 -= ADCA.CH2.RES;
+    ADCA.CH2.MUXCTRL = ADC_CH_MUXPOS_PIN6_gc | ADC_CH_MUXNEG_PIN3_gc;
+  }
+  else 
+  {                         //first (odd) measurement
+    sum_d69 += ADCA.CH2.RES;
+    ADCA.CH2.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_PIN6_gc;
+  }
+
+  n_d69++;
+  if (n_d69 == SAMPLES) 
+  {							//if eight measurement have been made safe it as a result
+    res_d69 = sum_d69/SAMPLES;
+    sum_d69 = 0;
+    n_d69 = 0;
+  }
+}
+
+//Interrupt service routine for d66
+ISR(ADCB_CH0_vect)
+{
+  static uint8_t n_d66 = 0;
+  static int16_t sum_d66 = 0;	
+
+  if(n_d66 & 0x01) 
+  {                  		//second (even) measurement
+    sum_d66 -= ADCB.CH0.RES;
+    ADCB.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN0_gc | ADC_CH_MUXNEG_PIN3_gc;
+  } 
+  else 
+  {                         //first (odd) measurement
+    sum_d66 += ADCB.CH0.RES;
+    ADCB.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_PIN0_gc;
+  }
+
+  n_d66++;
+  if(n_d66 == SAMPLES) 
+  {							//if eight measurement have been made safe it as a result
+    res_d66 = sum_d66/SAMPLES;
+    sum_d66 = 0;
+    n_d66 = 0;
+  }
+}
+
+//Interrupt service routine for d63
+ISR(ADCB_CH1_vect)
+{
+  static uint8_t n_d63 = 0;
+  static int16_t sum_d63 = 0;
+
+  if(n_d63 & 0x01) 
+  {                  		//second (even) measurement
+    sum_d63 -= ADCB.CH1.RES;
+    ADCB.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN2_gc | ADC_CH_MUXNEG_PIN3_gc;
+  } 
+  else 
+  {                         //first (odd) measurement
+    sum_d63 += ADCB.CH1.RES;
+    ADCB.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_PIN2_gc;
+  }
+
+  n_d63++;
+  if(n_d63 == SAMPLES)
+  {							//if eight measurement have been made safe it as a result
+    res_d63 = sum_d63/SAMPLES;
+    sum_d63 = 0;
+    n_d63 = 0;
+  }
+}
+
+//Interrupt service routine for d60
+ISR(ADCB_CH2_vect)
+{
+  static uint8_t n_d60 = 0;
+  static int16_t sum_d60 = 0;
+
+  if(n_d60 & 0x01) 
+  {                  		//second (even) measurement
+    sum_d60 -= ADCB.CH2.RES;
+    ADCB.CH2.MUXCTRL = ADC_CH_MUXPOS_PIN5_gc | ADC_CH_MUXNEG_PIN3_gc;
+  }
+  else 
+  {                         //first (odd) measurement
+    sum_d60 += ADCB.CH2.RES;
+    ADCB.CH2.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_PIN5_gc;
+  }
+
+  n_d60++;
+  if (n_d60 == SAMPLES) 
+  {							//if eight measurement have been made safe it as a result
+    res_d60 = sum_d60/SAMPLES;
+    sum_d60 = 0;
+    n_d60 = 0;
+  }
+}
+
+//Interrupt service routine for d57
+ISR(ADCB_CH3_vect)
+{
+  static uint8_t n_d57 = 0;
+  static int16_t sum_d57 = 0;
+
+  if(n_d57 & 0x01) 
+  {                  		//second (even) measurement
+    sum_d57 -= ADCB.CH3.RES;
+    ADCB.CH3.MUXCTRL = ADC_CH_MUXPOS_PIN7_gc | ADC_CH_MUXNEG_PIN3_gc;
+  }
+  else 
+  {                         //first (odd) measurement
+    sum_d57 += ADCB.CH3.RES;
+    ADCB.CH3.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_PIN7_gc;
+  }
+
+  n_d57++;
+  if (n_d57 == SAMPLES) 
+  {							//if eight measurement have been made safe it as a result
+    res_d57 = sum_d57/SAMPLES;
+    sum_d57 = 0;
+    n_d57 = 0;
+  }
 }
 
 void init_Line_Follower(void)
 {
-	PORTE_DIRCLR = PIN7_bm;		// SIN
-	PORTE_DIRSET = PIN6_bm;		// SOUT
+	PORTE_DIRSET = PIN7_bm;		// SIN
+	PORTE_DIRCLR = PIN6_bm;		// SOUT
 	PORTE_DIRSET = PIN5_bm;		// SCLK
 	PORTE_DIRSET = PIN4_bm;		// LAT
 	PORTE_DIRSET = PIN3_bm;		// BLANK
@@ -94,24 +228,91 @@ void init_Line_Follower(void)
 	PORTE_OUTSET = PIN3_bm;		// BLANK high
 	
 	// 96 pulses on SCLK
-	for (int i = 0; i < 96; i++) {
+	for (int i = 0; i < 96; i++) 
+	{
 		PORTE_OUTSET = PIN5_bm;
 		PORTE_OUTCLR = PIN5_bm;
 	}
 	
 	// 1 puls op LAT
-	PORTE_OUTTGL = PIN4_bm;
-	PORTE_OUTTGL = PIN4_bm;
+	PORTE_OUTSET = PIN4_bm;
+	PORTE_OUTCLR = PIN4_bm;
 	
 	////////////////////////////////////////
 
-	PORTA.DIRCLR     = PIN0_bm|PIN1_bm|PIN2_bm|PIN3_bm|PIN4_bm|PIN5_bm|PIN6_bm|PIN7_bm;		//configure PA as input for ADCA
-	ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN0_gc | ADC_CH_MUXNEG_PIN3_gc;						//PA0 to channel 0
+	PORTA.DIRCLR     = PIN1_bm|PIN3_bm|PIN4_bm|PIN6_bm;										//configure PA as input for ADCA
+
+	ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN1_gc | ADC_CH_MUXNEG_PIN3_gc;						//PA0 to channel 0
 	ADCA.CH0.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;											//differential, no gain
+
+	ADCA.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN4_gc | ADC_CH_MUXNEG_PIN3_gc;						//PA1 to channel 1
+	ADCA.CH1.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;											//differential, no gain
+
+	ADCA.CH2.MUXCTRL = ADC_CH_MUXPOS_PIN6_gc | ADC_CH_MUXNEG_PIN3_gc;						//PA5 to channel 2
+	ADCA.CH2.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;											//differential, no gain
 
 	ADCA.CTRLB       = ADC_RESOLUTION_12BIT_gc | ADC_CONMODE_bm; 							//
 
-	ADCA.REFCTRL     = ADC_REFSEL_INTVCC_gc;												//internal vcc/1.6 refernce
+	ADCA.REFCTRL     = ADC_REFSEL_INTVCC2_gc;												//internal vcc/2 refernce
 	ADCA.PRESCALER   = ADC_PRESCALER_DIV16_gc;												//prescaling
 	ADCA.CTRLA       = ADC_ENABLE_bm;														//enable ADC
+
+	ADCA.EVCTRL		 = ADC_SWEEP_012_gc | ADC_EVSEL_0123_gc | ADC_EVACT_CH012_gc;			//Sweep CH0,1,2; select event CH0,1,2,3; event triggers ADC CH0,1,2
+
+	////////////////////////////////////////
+
+	PORTB.DIRCLR     = PIN0_bm|PIN2_bm|PIN3_bm|PIN5_bm|PIN7_bm;								//configure PB as input for ADCB
+
+	ADCB.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN0_gc | ADC_CH_MUXNEG_PIN3_gc;						//PB0 to channel 0
+	ADCB.CH0.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;											//differential, no gain
+
+	ADCB.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN2_gc | ADC_CH_MUXNEG_PIN3_gc;						//PB1 to channel 1
+	ADCB.CH1.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;											//differential, no gain
+
+	ADCB.CH2.MUXCTRL = ADC_CH_MUXPOS_PIN5_gc | ADC_CH_MUXNEG_PIN3_gc;						//PB5 to channel 2
+	ADCB.CH2.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;											//differential, no gain
+
+	ADCB.CH3.MUXCTRL = ADC_CH_MUXPOS_PIN7_gc | ADC_CH_MUXNEG_PIN3_gc;						//PB6 to channel 3
+	ADCB.CH3.CTRL    = ADC_CH_INPUTMODE_DIFF_gc;											//differential, no gain
+
+	ADCB.CTRLB       = ADC_RESOLUTION_12BIT_gc | ADC_CONMODE_bm; 							//
+
+	ADCB.REFCTRL     = ADC_REFSEL_INTVCC2_gc;												//internal vcc/2 refernce
+	ADCB.PRESCALER   = ADC_PRESCALER_DIV16_gc;												//prescaling
+	ADCB.CTRLA       = ADC_ENABLE_bm;														//enable ADC
+
+	ADCB.EVCTRL		 = ADC_SWEEP_0123_gc | ADC_EVSEL_0123_gc | ADC_EVACT_CH0123_gc;			//Sweep CH0,1,2,3; select event CH0,1,2,3; event triggers ADC CH0,1,2,3
+
+	////////////////////////////////////////
+
+	color_Line_Follower();																	//set the line follower color
+
+	EVSYS.CH0MUX	 = EVSYS_CHMUX_TCE0_OVF_gc;												//event overflow timer E0 CH0
+	PMIC.CTRL |= PMIC_LOLVLEN_bm;															//turn on low level interrupts
+}
+
+void init_Timer(void)
+{
+  TCE0.PER      = 31249;     					// Tper =  8 * (3124 +1) / 32M = 0.025 s
+  TCE0.CTRLA    = TC_CLKSEL_DIV256_gc;          // Prescaling 8
+  TCE0.CTRLB    = TC_WGMODE_NORMAL_gc;        	// Normal mode
+  TCE0.INTCTRLA = TC_OVFINTLVL_OFF_gc;        	// Interrupt overflow off
+}
+
+void color_Line_Follower(void)
+{
+	for (int i = 0; i < 96; i++) 
+	{
+		if (led_out_blue[i] == 1) PORTE_OUTSET = PIN7_bm;
+		else PORTE_OUTCLR = PIN7_bm;
+		
+		PORTE_OUTSET = PIN5_bm;
+		PORTE_OUTCLR = PIN5_bm;
+	}
+	
+	// 1 puls op LAT
+	PORTE_OUTSET = PIN4_bm;
+	PORTE_OUTCLR = PIN4_bm;
+	
+	PORTE_OUTCLR = PIN3_bm;		// BLANK low
 }
