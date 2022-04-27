12939407898ed838ff3efc7a15d75e38243437f6
Julian Della Guardia <juliandellaguardia@hotmail.com>
2022-04-27 15:47:10 +0200

    test_COM werkt nu

--- Not compiled/test_COM.cpp
+++ Not compiled/test_COM.cpp
@@ -1,8 +1,9 @@
+//g++ -Wall -o COM test_COM.cpp ~/hva_libraries/legorpi/*.cpp -I/home/piuser/hva_libraries/legorpi ~/hva_libraries/legorpi/*.c
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <legorpi.hpp>
 #include <rs232.h>
 
 #define BUFSZ 4096
@@ -12,35 +13,40 @@
 #define INVALIDDATA -1
 #define AANTAL_WAARDES 7
 
-// Create a BrickPi3 instance with the default address of 1
-BrickPi3 oLego;
-
 //Method to catch the CTRL+C signal.
 void exit_signal_handler(int signo);
 //Get new data form the xMega
 int GetNewXMegaData(int *data_Location, int data_Size);
-
 // Start of the program
 int main(int nArgc, char* aArgv[]) {
   // Register the exit function for Ctrl+C
   signal(SIGINT, exit_signal_handler);
-  printf("Test1");
-  // Make sure that the BrickPi3 is communicating and the firmware is working as expected.
-  oLego.isDetected();
   
   int waarde[AANTAL_WAARDES];
-   
-   while(true) {
-     int regelResult = GetNewXMegaData(waarde, AANTAL_WAARDES);
-     if(regelResult == VALIDDATA) {
+  int commIsOpen = 0;
+
+  commIsOpen = !RS232_OpenComport(COMPORT, 115200, "8N1", 0);
+  while(1) 
+  { 
+    if (!commIsOpen) 
+    {
+    printf("Can not open COM %d\nExit Program\n", COMPORT);
+    exit(-2);
+    }
+    if(commIsOpen)
+    {
+      int regelResult = GetNewXMegaData(waarde, AANTAL_WAARDES);
+      if(regelResult == VALIDDATA) 
+      {
         printf("Succes! Waardes zijn:");
-        for(int i = 0; i < AANTAL_WAARDES; i++)
-        printf(" %d", waarde[i]);
+        for(int i = 0; i < AANTAL_WAARDES; i++) printf(" %d", waarde[i]);
         printf("\n");
-     } else if(regelResult == INVALIDDATA) {
-        printf("Error");
-     };/* en als regelResult == 0, dan was er nog geen nieuwe regel binnen */
-   };
+      } 
+      else if(regelResult == INVALIDDATA) printf("Error\n Data niet goed ontvangen!\n"); //en als regelResult == 0, dan was er nog geen nieuwe regel binnen
+      
+    }
+    //sleep(1);
+  };
 };
 
 // Signal handler that will be called when Ctrl+C is pressed to stop the program
@@ -48,58 +54,52 @@ void exit_signal_handler(int signo) {
 
   if (signo == SIGINT) {
     // Reset everything so there are no run-away motors
-    oLego.reset_all();
     printf("\nThe LEGO RPi example has stopped\n\n");
     exit(-2);
-  };
+  }
 };
 
-//Get new data form the xMega
-int GetNewXMegaData(int *data_Location, int data_Size) 
-{
+int GetNewXMegaData(int *data_Location, int data_Size) {
   static char sCommBuf[BUFSZ];
-  static char sCommBufLen = 0;
+  static int sCommBufLen = 0;
   int bytesRead = 0, valid = 1, lineRead = 0;
   
-  do 
-  {
-    bytesRead = RS232_PollComport(COMPORT, (unsigned char *) &sCommBuf[sCommBufLen], BUFSZ - sCommBufLen - 1);
-    if(bytesRead > 0) 
-    {
+  do {
+    bytesRead = RS232_PollComport(COMPORT, (unsigned char *) &sCommBuf[sCommBufLen], 1);
+    if(bytesRead > 0) {
       sCommBufLen += bytesRead;
       sCommBuf[sCommBufLen] = '\0';
-      char *endOfLine = strchr(sCommBuf, '\n');
-      if(endOfLine != NULL)
-      {
+      if(sCommBuf[sCommBufLen - 1] == '\n') {
         char *here = sCommBuf;
-        for(int i = 0; i < data_Size && valid == 1; i++) 
-        {
-          while(isspace(*here) && here != endOfLine) here++;
-          if(isdigit(*here)) data_Location[i] = strtol(here, &here, 10);
-          else valid = 0;
-        } // for
-        if(valid == 1) 
-        {
-          while(isspace(*here) && here != endOfLine)
+        for(int i = 0; i < data_Size && valid == 1; i++) {
+          while(isspace(*here))
             here++;
-          if(here != endOfLine)
+          if(isdigit(*here))
+            data_Location[i] = strtol(here, &here, 10);
+          else
+            valid = 0;
+        } // for
+        if(valid == 1) {
+          while(isspace(*here))
+            here++;
+          if(*here != '\0')
             valid = 0;
           else
             lineRead = 1;
         }
-        memmove(sCommBuf, endOfLine + 1, strlen(endOfLine + 1) + 1);
-        sCommBufLen = strlen(sCommBuf);
-      } // if(endOfLine
-      else if(sCommBufLen >= BUFSZ - 1) 
-      {
+        sCommBufLen = 0;
+      } // if(sCommBuf
+      else if(sCommBufLen >= BUFSZ - 1) {
         sCommBufLen = 0;
         valid = 0;
       }
     } // if(bytesRead
-  } 
-  while(bytesRead > 0 && lineRead == 0 && valid == 1);
+  } while(bytesRead > 0 && lineRead == 0 && valid == 1);
 
-  if(bytesRead == 0)        return NODATA;
-  else if(lineRead == 1)    return VALIDDATA;
-  else                      return INVALIDDATA;
+  if(bytesRead == 0)
+    return NODATA;
+  else if(lineRead == 1)
+    return VALIDDATA;
+  else
+    return INVALIDDATA;
 }
